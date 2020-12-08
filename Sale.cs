279e6b82e20a48e3b6c64414de1615466474b4b7
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-08 13:39:00 +0300

    Add files via upload

---
+++ Sale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApplication
+{
+    class Sale
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество минут разговора");
+            int CountOfMinutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите стоимость минуты");
+            double MinutePrice = Convert.ToDouble(Console.ReadLine());
+
+            double FullPrice = (double)(CountOfMinutes * MinutePrice);
+
+            Console.WriteLine("Введите номер дня недели");
+            int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+            double WeekendSale = (double)0.2;
+
+            if (!(DayOfWeek == 0 || DayOfWeek == 6))
+            {
+                Console.WriteLine(FullPrice);
+            }
+            else
+            {
+                Console.WriteLine(FullPrice - (FullPrice * WeekendSale));
+            }
+        }
+
+    }
+}
