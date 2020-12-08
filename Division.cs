c983daadde3fe7dfaf7883dfefc29e7ee56c1ac5
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-08 15:42:23 +0300

    Add files via upload

---
+++ Division.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApplication
+{
+    class Division
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int a = r.Next(100);
+            Console.WriteLine("Значение a: " + a);
+
+            int b = r.Next(100);
+            Console.WriteLine("Значение b: " + b);
+
+            Console.WriteLine("Введите ответ: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a / b == c)
+            {
+                Console.WriteLine("Ответ верный!");
+            }
+            else
+            {
+                Console.WriteLine("Ответ не верный.");
+                Console.WriteLine("Правильный ответ: " + (a / b));
+            }
+        }
+    }
+}
