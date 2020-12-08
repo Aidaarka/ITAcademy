b305e2846fdbdf2151c353fd758832c44a3bfc22
Aidaarka <66637129+Aidaarka@users.noreply.github.com>
2020-12-08 14:04:35 +0300

    Add files via upload

---
+++ SecondWorldWar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApplication
+{
+    class SecondWorldWar
+    {
+        static void Main(string[] args)
+        {
+            DateTime SecondWWDate = new DateTime(1939, 09, 1);
+
+            Console.WriteLine("Введите дату начала Второй мировой войны \" дд.мм.гггг \"");
+
+            DateTime YourVersion = Convert.ToDateTime(Console.ReadLine());
+
+            if (!(YourVersion == SecondWWDate))
+            {
+                Console.WriteLine("Не верно!");
+                Console.WriteLine("Правильный ответ " + SecondWWDate.ToShortDateString());
+            }
+            else
+            {
+                Console.WriteLine("Верно!");
+            }
+        }
+    }
+}
